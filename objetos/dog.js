8bd3a74e94230df2389a0eb3ffa4d9ac18fce0d7
Maria de Lourdes Carvalho Chaves <95364349+maluchaves91@users.noreply.github.com>
2024-01-31 16:23:40 -0300

    31.01

--- objetos/dog.js
+++ objetos/dog.js
@@ -3,23 +3,24 @@ class Dog {
         this.w = width / 7;
         this.h = height / 8;
         this.x = 50;
-        this.y = height - 1.2*this.h;
+        this.y = height - 1.2 * this.h;
         this.g = 5;
-this.vy=0
+        this.vy = 0;
     }
-move (){
-this.y+=this.vy;
-this.vy+=this.g;
-this.y=constrain(this.y,0,height-this.h);
-}
+
+    move() {
+        this.y += this.vy;
+        this.vy += this.g;
+        this.y = constrain(this.y, 0, height - this.h);
+    }
 
     show() {
         image(img_dog, this.x, this.y, this.w, this.h);
     }
 
-    jump(){
-       if(this.y == height-this.h){
-        this.vy=-35
-       }
+    jump() {
+        if (this.y == height - this.h) {
+            this.vy = -35;
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,31 +1,30 @@
 let dog;
 let img_btn_iniciar, img_dog, img_fundo, img_inicio, img_moeda, img_perdeu, img_venceu, img_obstaculo;
 
-function preload(){
-  img_btn_iniciar=loadImage("imagens/btn_iniciar.png");
-  img_dog=loadImage("imagens/dog.gif");
-  img_fundo=loadImage("imagens/fundo.gif");
-  img_inicio=loadImage("imagens/inicio.gif");
-  img_moeda=loadImage("imagens/moeda.gif");
-  img_perdeu=loadImage("imagens/perdeu.png");
-  img_venceu=loadImage("imagens/venceu.png");
-  img_obstaculo=loadImage("imagens/obstaculo.png")
+function preload() {
+  img_btn_iniciar = loadImage("imagens/btn_iniciar.png");
+  img_dog = loadImage("imagens/dog.gif");
+  img_fundo = loadImage("imagens/fundo.gif");
+  img_inicio = loadImage("imagens/inicio.gif");
+  img_moeda = loadImage("imagens/moeda.gif");
+  img_perdeu = loadImage("imagens/perdeu.png");
+  img_venceu = loadImage("imagens/venceu.png");
+  img_obstaculo = loadImage("imagens/obstaculo.png");
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    dog=new Dog();
-  }
-  
-  function draw() {
-    background(220);
-    dog.show();
-  dog.move();
+  createCanvas(windowWidth, windowHeight);
+  dog = new Dog();
+}
 
-  
-  function keyPressed(){
-    if(Key=" "){
-      dog.jump();
-    }
+function draw() {
+  background(220);
+  dog.show();
+  dog.move();
+}
+
+function keyPressed(){
+  if (key == " ") {
+    dog.jump();
   }
 }
